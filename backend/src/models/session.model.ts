aa4a4c62e8a9ecbd4eb134c496bbec384c130850
DAG WEG <dagtef2@gmail.com>
2024-05-11 16:07:38 +0300

    session model

---
+++ backend/src/models/session.model.ts
@@ -0,0 +1,13 @@
+import { ObjectId } from "mongodb";
+import mongoose, { InferSchemaType, mongo } from "mongoose";
+import { optional } from "zod";
+import { dateHoursFromNow } from "../utils/date";
+
+const sessionSchema = new mongoose.Schema({
+  userId: { type: ObjectId, unique: true, ref: "User" },
+  sessionToken: { type: String, required: true },
+  expires: { type: Date, optional: true, default: dateHoursFromNow(24) },
+});
+
+export type SessionType = InferSchemaType<typeof sessionSchema>;
+export const Session = mongoose.model<SessionType>("Session", sessionSchema);
