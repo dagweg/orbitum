5ca012bd7dcf78590140940ec398589ad0cc88ee
DAG WEG <dagtef2@gmail.com>
2024-05-11 22:42:30 +0300

    chore: userSlice

---
+++ frontend/lib/redux/slices/userSlice.ts
@@ -0,0 +1,26 @@
+import { PayloadAction, Tuple, createSlice } from "@reduxjs/toolkit";
+
+type TUser = {
+  email?: string;
+  sessionId?: string;
+};
+
+const initialState: TUser = {
+  email: undefined,
+  sessionId: undefined,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUserEmail(state, action: PayloadAction<string | undefined>) {
+      state.email = action.payload;
+    },
+    setUserSession(state, action: PayloadAction<string | undefined>) {
+      state.sessionId = action.payload;
+    },
+  },
+});
+
+export const User = userSlice.reducer;
