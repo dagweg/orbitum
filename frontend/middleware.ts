2eb8e0b59346facee4d14485da66b46f9b0f8914
DAG WEG <dagtef2@gmail.com>
2024-05-12 17:37:44 +0300

    em

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,8 +1,3 @@
 import { NextRequest, NextResponse } from "next/server";
-// import otpPageMiddleware from "./middlewares/otpPageMiddleware";
 
-export default function middleware(req: NextRequest, res: NextResponse) {
-  // otpPageMiddleware(req, res);
-}
-
-// export const protectedRoutes = ["/auth/otp"];
+export default function middleware(req: NextRequest, res: NextResponse) {}
