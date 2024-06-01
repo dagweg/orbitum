21f7a876db2f56062eaa96572eee75494510f7d2
Dagmawi <dagtef2@gmail.com>
2024-06-01 10:16:12 +0300

    feat: added you identifier

--- backend/src/routes/chat/getPrivateChat.ts
+++ backend/src/routes/chat/getPrivateChat.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { PrivateChat } from "../../models/private_chat.model";
-import { Message } from "../../models/message.model";
+import { Message, MessageDocument } from "../../models/message.model";
 
 export async function getPrivateChat(req: Request, res: Response) {
   try {
@@ -9,7 +9,7 @@ export async function getPrivateChat(req: Request, res: Response) {
     console.log("User 1 id ", userId);
     console.log("User 2 id ", user2Id);
 
-    let chat = await PrivateChat.find({
+    let chats = await PrivateChat.find({
       $or: [
         { user1: userId, user2: user2Id },
         { user2: userId, user1: user2Id },
@@ -20,12 +20,20 @@ export async function getPrivateChat(req: Request, res: Response) {
         populate: {
           path: "sender",
           select: "firstName lastName userName email _id profileUrl",
+          // how can I add a custom field you : boolean that specifies if the sender is you or not to later be used in the client side for correct rendering. since I'm using httponly cookie I cant' access user information in the lcient side
         },
       })
       .populate("user1")
       .populate("user2");
 
-    return res.json(chat);
+    let ret = chats.map((chat) =>
+      chat.toObject().messages.map((message: any) => ({
+        ...message,
+        you: message.sender._id.toString() === userId.toString(),
+      }))
+    );
+
+    return res.json(ret.flat(1));
   } catch (error) {
     console.log((error as Error).message);
     return res.status(500).json(error);
