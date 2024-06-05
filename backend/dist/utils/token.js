a411269a71c412492d64a02782399d0a11c775b0
= <dagmawi.wegayehu-ug@aau.edu.et>
2024-06-06 01:05:25 +0300

    nothing

--- backend/dist/utils/token.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateToken = void 0;
-const crypto = require("crypto");
-function generateToken(length = 64) {
-    return crypto.randomBytes(length).toString("hex");
-}
-exports.generateToken = generateToken;
--- backend/dist/validators/otpVerify.validation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OTPVerifySchema = void 0;
-const zod_1 = require("zod");
-exports.OTPVerifySchema = zod_1.z.object({
-    inputOtp: zod_1.z.string(),
-    token: zod_1.z.string(),
-});
