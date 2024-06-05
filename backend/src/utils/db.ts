bcf53f8058f30fc7f9c56ad533184a9d67329cbd
= <dagmawi.wegayehu-ug@aau.edu.et>
2024-06-06 00:38:05 +0300

    feat: mongo atlas connection for render depl setup

--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -1,12 +1,24 @@
+import { ServerApiVersion } from "mongodb";
 import mongoose from "mongoose";
 
 function connectDB() {
-  const uri = process.env.MONGO_URI;
-
+  const uri = process.env.MONGO_URI as string;
+  console.log(uri);
   mongoose
-    .connect(uri as string)
-    .then(() => {})
-    .catch((err) => {});
+    .connect(uri as string, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+      autoIndex: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB Atlas");
+    })
+    .catch((err) => {
+      console.error("Error connecting to MongoDB:", err);
+    });
 }
 
 export default connectDB;
