832c0e1228d0e988fff079f636c7b0a67a9205c6
= <dagmawi.wegayehu-ug@aau.edu.et>
2024-06-17 23:35:31 +0300

    stylix:

--- frontend/app/components/chat-text-area.tsx
+++ frontend/app/components/chat-text-area.tsx
@@ -1,8 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Mic, Paperclip, SendHorizontal, Smile } from "lucide-react";
-import React, { Ref } from "react";
+import React, { Ref, useState } from "react";
 import TextAreaAutoSize from "react-textarea-autosize";
-import { boolean } from "zod";
+import EmojiPicker from "emoji-picker-react";
 
 function ChatTextArea({
   chatTextAreaRef,
@@ -21,39 +21,53 @@ function ChatTextArea({
   };
   handleMessageSend: () => void;
 }) {
+  const [emojiPane, setEmojiPane] = useState({
+    enabled: false,
+  });
+
   return (
-    <section className=" pb-2     bottom-0 sticky w-full  mx-auto flex flex-col-reverse bg-neutral-200 pt-4 h-fit px-10">
-      <div className="flex items-center justify-around gap-4 bg-white w-full p-2 mx-auto ring-1 max-w-[700px]  ring-neutral-400  rounded-lg">
-        <div className="h-full flex flex-col-reverse items-end">
-          <Button variant={"circleGhost"}>
-            <Paperclip className="" />
-          </Button>
-        </div>
-        <TextAreaAutoSize
-          maxRows={10}
-          ref={chatTextAreaRef}
-          className=" outline-none rounded-lg no-scrollbar md:scrollbar resize-none"
-          placeholder="Type a message"
-          onChange={(e) => handleTextAreaChange(e)}
-          onKeyDown={(e) => handleTextAreaKeyDown(e)}
-          onKeyUp={(e) => handleTextAreaKeyUp(e)}
-          style={{ width: "100%" }}
-        />
-        <div className="h-full flex flex-col-reverse items-end">
-          <div className="flex items-end">
-            <Button variant={"circleGhost"}>
-              <Smile />
-            </Button>
-            <Button
-              onClick={hasStartedTyping.you ? handleMessageSend : () => {}}
-              variant={"circleGhost"}
-            >
-              {hasStartedTyping.you ? <SendHorizontal /> : <Mic />}
-            </Button>
+    <>
+      <div className="flex flex-col w-full  sticky  bottom-0 ">
+        {emojiPane.enabled && (
+          <div className="relative max-w-[700px] w-full mx-auto">
+            <EmojiPicker className="!absolute bottom-0 right-0 z-[1000]" />
           </div>
-        </div>
+        )}
+        <section className=" pb-2     w-full  mx-auto flex flex-col-reverse items-end gap-3 bg-neutral-200 pt-4 h-fit px-10">
+          <div className="h-fit flex items-center justify-around gap-4 bg-white w-full p-2 mx-auto ring-1 max-w-[700px]  ring-neutral-400  rounded-lg">
+            <div className="h-full flex flex-col-reverse items-end">
+              <Button variant={"circleGhost"}>
+                <Paperclip className="" />
+              </Button>
+            </div>
+
+            <TextAreaAutoSize
+              maxRows={5}
+              ref={chatTextAreaRef}
+              className=" outline-none rounded-lg no-scrollbar md:scrollbar resize-none"
+              placeholder="Type a message"
+              onChange={(e) => handleTextAreaChange(e)}
+              onKeyDown={(e) => handleTextAreaKeyDown(e)}
+              onKeyUp={(e) => handleTextAreaKeyUp(e)}
+              style={{ width: "100%" }}
+            />
+            <div className="h-full flex flex-col-reverse items-end">
+              <div className="flex items-end">
+                <Button variant={"circleGhost"}>
+                  <Smile />
+                </Button>
+                <Button
+                  onClick={hasStartedTyping.you ? handleMessageSend : () => {}}
+                  variant={"circleGhost"}
+                >
+                  {hasStartedTyping.you ? <SendHorizontal /> : <Mic />}
+                </Button>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
-    </section>
+    </>
   );
 }
 
