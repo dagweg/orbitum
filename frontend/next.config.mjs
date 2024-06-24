076a1044c7feaf7aa243ca3b020e958ab7f48138
= <dagmawi.wegayehu-ug@aau.edu.et>
2024-06-24 17:29:38 +0300

    next config env

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["imgur.com", "tenor.com", "i.pinimg.com"],
   },
+  env: {
+    API_ORIGIN: process.env.API_ORIGIN,
+  },
 };
 
 export default nextConfig;
