68db9a04955d5c5927025278bcdbdd0f760a704c
= <dagmawi.wegayehu-ug@aau.edu.et>
2024-06-24 16:34:52 +0300

    nullish

--- frontend/app/config/apiConfig.ts
+++ frontend/app/config/apiConfig.ts
@@ -2,6 +2,6 @@
  * Change the below constants to reflect the acutal deployment urls
  */
 export const API_ORIGIN: string =
-  process.env.API_ORIGIN || "http://localhost:5000";
+  process.env.API_ORIGIN ?? "http://localhost:5000";
 export const CLIENT_ORIGIN: string =
-  process.env.NEXT_PUBLIC_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_URL ?? "http://localhost:3000";
