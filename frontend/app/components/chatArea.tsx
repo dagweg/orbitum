318c219bd34a165ef8fec718f59326186c3f8aa0
= <dagmawi.wegayehu-ug@aau.edu.et>
2024-06-25 16:23:36 +0300

    refactor: chatArea chatInput chatTextarea

--- frontend/app/components/chatArea.tsx
+++ frontend/app/components/chatArea.tsx
@@ -1,39 +1,13 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import ChatMessage from "./chat-message";
-import { AppDispatch, RootState } from "@/lib/redux/store";
-import { useDispatch, useSelector } from "react-redux";
-import TextAreaAutoSize from "react-textarea-autosize";
-import {
-  Circle,
-  CircleDot,
-  Dot,
-  HelpCircleIcon,
-  Mic,
-  Paperclip,
-  SendHorizontal,
-  Smile,
-} from "lucide-react";
-import { ChangeEvent, useEffect, useRef, useState } from "react";
-import { Badge } from "@/components/ui/badge";
-import { TMessageSchema } from "@/lib/types/schema";
-import useSocket from "../hooks/useSocket";
-import socket from "@/lib/socket";
-import { setCurrentChat } from "@/lib/redux/slices/chat/chatThunks";
-import Image from "next/image";
-
-import abstract from "@/public/images/chat/abstract.jpg";
-import { Button } from "@/components/ui/button";
-
-import { PulseLoader, SyncLoader } from "react-spinners";
-import OnlineIndicator from "./online-indicator";
-import ChatWallpaper from "./chat-wallpaper";
+import { RootState } from "@/lib/redux/store";
+import { useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
 import ChatHeader from "./chat-header";
-import ChatTextArea from "./chat-text-area";
 import ChatMessages from "./chat-messages";
-import { createUrl } from "@/util/file";
 import { useChatSocket } from "../hooks/useChatSocket";
+import { ChatInput } from "./chat-input";
 
 function ChatArea() {
   const chatArea = useSelector((state: RootState) => state.ChatArea);
@@ -41,10 +15,7 @@ function ChatArea() {
     (state: RootState) => state.ChatSideBar.chat
   );
 
-  const dispatch = useDispatch<AppDispatch>();
-
-  const { hasStartedTyping, refreshChat, setHasStartedTyping } =
-    useChatSocket();
+  const { hasStartedTyping } = useChatSocket();
 
   const messages = chatArea.currentChat
     ? chatArea.currentChat.messages
@@ -52,90 +23,9 @@ function ChatArea() {
 
   const [message, setMessage] = useState<string>("");
 
-  const chatTextAreaRef = useRef<HTMLTextAreaElement>(null);
   const chatMessagesRef = useRef<HTMLDivElement>(null);
 
-  let keys = {
-    enter: false,
-    leftShift: false,
-  };
-
-  const typingTimeout = useRef(setTimeout(() => {}));
-
   let recipient = chatArea.currentChat?.recipient;
-
-  function handleTextAreaChange(e: ChangeEvent<HTMLTextAreaElement>) {
-    setMessage(e.currentTarget.value);
-    if (e.currentTarget.value.length > 0) {
-      setHasStartedTyping({ ...hasStartedTyping, you: true });
-    } else {
-      setHasStartedTyping({ ...hasStartedTyping, you: false });
-    }
-    socket.emit("chat:type", {
-      to: chatArea.currentChat?.recipientId,
-    });
-  }
-
-  function handleTextAreaKeyDown(e: React.KeyboardEvent<HTMLTextAreaElement>) {
-    if (e.code === "Enter") {
-      keys.enter = true;
-    }
-
-    if (e.code === "ShiftLeft") {
-      keys.leftShift = true;
-    }
-
-    if (keys.enter && keys.leftShift) {
-      // Send Message Here
-      handleMessageSend();
-      if (chatTextAreaRef && chatTextAreaRef.current)
-        chatTextAreaRef.current.value = "";
-    }
-  }
-
-  function handleTextAreaKeyUp(e: React.KeyboardEvent<HTMLTextAreaElement>) {
-    if (e.code === "Enter") {
-      keys.enter = false;
-    }
-
-    if (e.code === "ShiftLeft") {
-      keys.leftShift = false;
-    }
-  }
-
-  function handleMessageSend() {
-    console.log(message);
-    setMessage("");
-    if (chatArea.currentChat) {
-      socket.emit("chat:sendMessage", {
-        to: chatArea.currentChat.recipientId,
-        message,
-      });
-
-      setTimeout(() => refreshChat(), 50);
-    }
-  }
-
-  function handleMicRecord() {}
-
-  useSocket("chat:receiveMessage", ({ from, message }) => {
-    console.log("You have recieved message: ", message, " from ", from);
-    refreshChat();
-  });
-
-  useSocket("chat:type", (from) => {
-    console.log("tyPING", from);
-
-    if (typingTimeout.current) {
-      clearTimeout(typingTimeout.current);
-    }
-
-    setHasStartedTyping((prev) => ({ ...prev, recipient: true }));
-
-    typingTimeout.current = setTimeout(() => {
-      setHasStartedTyping((prev) => ({ ...prev, recipient: false }));
-    }, 1000);
-  });
 
   useEffect(() => {
     if (chatMessagesRef.current) {
@@ -167,17 +57,7 @@ function ChatArea() {
               messages={messages}
             />
             {messages && (
-              <ChatTextArea
-                handleMicRecord={handleMicRecord}
-                message={message}
-                setMessage={setMessage}
-                chatTextAreaRef={chatTextAreaRef}
-                handleMessageSend={handleMessageSend}
-                handleTextAreaChange={handleTextAreaChange}
-                handleTextAreaKeyDown={handleTextAreaKeyDown}
-                handleTextAreaKeyUp={handleTextAreaKeyUp}
-                hasStartedTyping={hasStartedTyping}
-              />
+              <ChatInput message={message} setMessage={setMessage} />
             )}
           </div>
         </div>
